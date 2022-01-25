ca42b241ac16726e948b8457455eeac07a9a72b1
Chrisahn75 <chris.ahona@gmail.com>
2022-01-25 17:24:38 +0100

    part user

--- users/src/App.js
+++ users/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./users.json"
+import "./users.json";
+import "./components/UserInfo";
 
+const data = "";
 
-class App extends React.Component {
-  
-}
+const App = () => {
+  const userList = data.map((data) => (
+    <p>
+      {data.name} - {data.email} - {data.website}
+    </p>
+  ));
 
+  return <p>{userList}</p>
+};
 export default App;
