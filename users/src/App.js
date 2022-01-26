4ad1e386c28b36ae70fe615c02be66a4d9d4ae68
Chrisahn75 <chris.ahona@gmail.com>
2022-01-26 16:31:55 +0100

    user

--- users/src/App.js
+++ users/src/App.js
@@ -4,12 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import users from "./users.json";
 import UserInfo from "./components/UserInfo.js";
 
-const userList = UsersInfo.map((data) => {
+const userList = userInfo.map((data) => {
   return (
     <UserInfo name={data.name} email={data.email} website={data.website}/>
   );
 });
-    return <div>{userList}</div>;
-
+  return <div>{userList}</div>;
 
 export default App;
